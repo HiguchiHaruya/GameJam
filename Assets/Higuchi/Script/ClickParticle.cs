f2095344bf42c1a9d898dcadfb8fe13d4c6aa154
HiguchiHaruya <tongkouyangye@gmail.com>
2024-05-22 12:57:58 +0900

    クリックエフェクトとロード画面アニメーションの追加

---
+++ Assets/Higuchi/Script/ClickParticle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickParticle : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _ClickEffect;
+    [SerializeField] private Camera _camera;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
+            _ClickEffect.transform.position = pos;
+            _ClickEffect.Play();
+        }
+    }
+}
